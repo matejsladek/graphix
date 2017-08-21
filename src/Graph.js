18f2631189dc0edbdfd3098b0c768ad032a820c3
Matej Sladek <matejsladek10@gmail.com>
2017-08-21 02:08:52 +0100

    add Graph functionality

--- src/Graph.js
+++ src/Graph.js
@@ -1,18 +1,150 @@
 class Graph {
   constructor(directed = true){
     this.directed = directed;
+    this.nodes = new Map();
+    this.nodeToId = new Map();
+    this.idToEdge = new Map();
+    this.idToNode = new Map();
+    this.highestNodeId = 0;
     this.edges = new Map();
+    this.highestEdgeId = 0;
+    this.nodeDefaultLabel = "";
+    this.edgeDefaultLabel = "";
   }
 
-  createEdge(vertexA, vertexB, value){
+  addNode(name, label = this.nodeDefaultLabel){
+    this.highestNodeId += 1;
+    const id = this.highestNodeId;
+    this.nodeToId.set(name, id);
+    this.idToNode.set(id, name);
+    this.nodes.set(id, label);
+    return this.highestNodeId;
+  }
+
+  hasNode(name){
+    return this.nodeToId.has(name);
+  }
+
+  getNodes(){
+    const nodes = [];
+    for(const id in nodes.keys()){
+      nodes.push(this.getNodeById(id));
+    }
+    return nodes;
+  }
+
+  getNode(name) {
+    return {
+      __id__: this.nodeToId.get(name),
+      name,
+      label: this.nodes.get(name),
+    }
+  }
+
+  getNodeById(id) {
+    return {
+      __id__: id,
+      name: this.idToNode.get(id),
+      label: this.nodes.get(name),
+    }
+  }
+
+  removeNode(name){
+    const id = this.nodeToId.get(name);
+    this.nodes.delete(id);
+    this.nodeToId.delete(id);
+    this.idToNode.delete(id);
+
+    this.edges.delete(id);
+    const keys = this.edges.keys();
+    for(const key of keys){
+      this.edges.get(key).delete(id);
+    }
+  }
+
+  createEdge(vertexA, vertexB, weight, id){
     if(!this.edges.has(vertexA)) this.edges.set(vertexA, new Map());
-    if(this.edges.get(vertexA).has(vertexB)) this.edges.get(vertexA).get(vertexB).push(value);
-    else this.edges.get(vertexA).set(vertexB, [value]);
+    const edge = {__id__: id, weight};
+    if(this.edges.get(vertexA).has(vertexB)) this.edges.get(vertexA).get(vertexB).push(edge);
+    else this.edges.get(vertexA).set(vertexB, [edge]);
   }
 
-  addEgde(vertexA, vertexB, value){
-    this.createEdge(vertexA, vertexB, value);
-    if(!this.directed) this.createEdge(vertexB, vertexA, value);
+  addEgde(vertexA, vertexB, weight=1, name="", label=this.edgeDefaultLabel){
+    if(!this.hasNode(vertexA)) this.addNode(vertexA);
+    if(!this.hasNode(vertexB)) this.addNode(vertexB);
+    const nodeIdA = this.nodeToId.get(vertexA);
+    const nodeIdB = this.nodeToId.get(vertexB);
+    this.highestEdgeId += 1;
+    const edgeId = this.highestEdgeId;
+    const edgeObj = {__id__: edgeId, name, label, weight};
+    this.idToEdge.set(edgeId, edgeObj);
+    this.createEdge(nodeIdA, nodeIdB, weight, edgeId);
+    if(!this.directed) this.createEdge(nodeIdB, nodeIdA, weight, edgeId);
+    return edgeObj;
+  }
+
+  getEdgeById(id){
+    return this.idToEdge(id);
+  }
+
+  getEdgeByName(name){
+    let result = [];
+    const keysA = this.edges.keys();
+    for(const keyA of keysA){
+      const keysB = this.edges.get(keyA).keys();
+      for(const keyB of keysB) {
+        const newArray = this.edges.get(keyA).get(keyB).filter((elem) => {
+          return elem.name === name
+        });
+        result = [...result, ...newArray];
+      }
+    }
+    return result;
+  }
+
+  removeEdge(nodeA, nodeB) {
+    const nodeIdA = this.nodeToId.get(nodeA);
+    const nodeIdB = this.nodeToId.get(nodeB);
+    this.edges.get(nodeIdA).delete(nodeIdB);
+    if (!this.directed) this.edges.get(nodeIdB).delete(nodeIdA);
+  }
+
+  removeEdgeByFunc(func){
+    const keysA = this.edges.keys();
+    for(const keyA of keysA){
+      const keysB = this.edges.get(keyA).keys();
+      for(const keyB of keysB) {
+        const newArray = this.edges.get(keyA).get(keyB).filter((elem) => {
+          if(func(elem)){
+            const id = this.edges.get(keyA).get(keyB).__id__;
+            this.idToEdge.delete(id);
+            return false;
+          }
+          return true;
+        });
+        this.edges.get(keyA).set(keyB, newArray);
+      }
+    }
+  }
+
+  removeEdgeById(id){
+    this.removeEdgeByFunc((elem) =>{
+      return elem.__id__ !== id;
+    });
+  }
+
+  removeEdgeByName(name){
+    this.removeEdgeByFunc((elem) =>{
+      return elem.name !== name;
+    });
+  }
+
+  setNodeDefaultLabel(label){
+    this.nodeDefaultLabel = label;
+  }
+
+  setEdgeDefaultLabel(label){
+    this.edgeDefaultLabel = label;
   }
 }
 
--- src/dijkstra.js
+++ src/dijkstra.js
@@ -1,22 +1,26 @@
 import { PriorityQueue } from 'es-collections';
 
 function dijkstra(graph, vertexFrom, vertexTo){
+  const start = graph.getNode(vertexFrom).__id__;
+  const finish = graph.getNode(vertexTo).__id__;
   const marked = new Set();
   let pq = new PriorityQueue((a, b) => a.dist - b.dist);
   const edges = graph.edges;
-  pq.add({name: vertexFrom, dist: 0});
+  pq.add({name: start, dist: 0});
   while(pq.size !== 0){
     const top = pq.remove();
     const currentName = top.name;
     const currentDist = top.dist;
     if(marked.has(currentName)) continue;
     marked.add(currentName);
-    if(currentName === vertexTo) return top.dist;
+    if(currentName === finish) return top.dist;
     if(edges.has(currentName)){
-      for (let [key, value] of edges.get(currentName)) {
-        const lowestPrice = Math.min(...value);
-        pq.add({ name: key, dist: currentDist + lowestPrice });
-      }
+      edges.get(currentName).forEach((value, key) => {
+        if(value.length === 0) return;
+        const weights = value.map(elem => elem.weight);
+        const lowestWeight = Math.min(...weights);
+        pq.add({ name: key, dist: currentDist + lowestWeight });
+      });
     }
   }
   return Infinity;
