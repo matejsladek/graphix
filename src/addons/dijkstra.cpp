22b2377833112d46587f872cb232524e8f2b8e41
Matej Sladek <matejsladek10@gmail.com>
2017-09-02 19:00:59 +0100

    c++ speed improvement

--- src/addons/dijkstra.cpp
+++ src/addons/dijkstra.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
 #include <iostream>
 #include <queue>
-#include <set>
+#include <unordered_set>
 #include "dijkstra.hpp"
 
 napi_value Dijkstra(napi_env env, napi_callback_info info) {
@@ -12,41 +12,35 @@ napi_value Dijkstra(napi_env env, napi_callback_info info) {
     status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
     int from, to;
-    std::map<int, std::map<int, int> > adj;
+    std::unordered_map<int, std::vector<std::pair<int, int>>> adj;
 
     status = napi_get_value_int32(env, args[1], &from);
     status = napi_get_value_int32(env, args[2], &to);
+
     size_t arrayLen;
-    void **data = nullptr;
-    status = napi_get_arraybuffer_info(env, args[0], data, &arrayLen);
-    void *realData;
-    realData = malloc(arrayLen * sizeof(int32_t));
-    status = napi_get_arraybuffer_info(env, args[0], &realData, &arrayLen);
+    void *data_input;
+    status = napi_get_arraybuffer_info(env, args[0], &data_input, &arrayLen);
     int numberOfElements = (int) arrayLen / (int) sizeof(int32_t);
-    auto edges = static_cast<int*> (realData);
-    for (int i = 0; i < numberOfElements; ++i) {
+    auto edges = static_cast<int*> (data_input);
+    for (int i = 0; i < numberOfElements; i += 3) {
         int edgeFrom = edges[i];
         int edgeTo = edges[i+1];
         int weight = edges[i+2];
-        if((adj[edgeFrom][edgeTo] && adj[edgeFrom][edgeTo] > weight) || !adj[edgeFrom][edgeTo]) {
-            adj[edgeFrom][edgeTo] = weight;
-        }
-        i += 2;
+        adj[edgeFrom].push_back({edgeTo, weight});
     }
 
-    std::vector<std::pair<int, int > > res = DijkstraImpl(adj, from, to);
-    int sz = res.size() * 2;
+    std::vector<int> distances;
+    DijkstraImpl(adj, from, to, distances);
+
+    int sz = distances.size();
     void *output;
-    output = malloc(sz * sizeof(int32_t));
     napi_value argv[1];
-//    status = napi_create_int32(env, res, argv);
     status = napi_create_arraybuffer(env, sz * sizeof(int32_t), &output, argv);
 
     auto realOutput = static_cast<int*> (output);
 
-    for (int i = 0; i < (int)res.size(); ++i) {
-        realOutput[i*2] = res[i].first;
-        realOutput[i*2+1] = res[i].second;
+    for (int i = 0; i < (int)distances.size(); ++i) {
+        realOutput[i] = distances[i];
     }
 
     napi_value global;
@@ -58,26 +52,33 @@ napi_value Dijkstra(napi_env env, napi_callback_info info) {
     return result;
 }
 
-std::vector<std::pair<int, int > > DijkstraImpl(std::map<int, std::map<int, int> > adj, int from, int to) {
-    std::vector<std::pair<int, int > > distances;
-    std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>> > pq;
-    std::set<int> marked;
+void DijkstraImpl(std::unordered_map<int, std::vector<std::pair<int, int>>>& adj, int from, int to, std::vector<int>& distances) {
+    std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> pq;
+    std::unordered_set<int> marked;
     pq.push({from, 0});
     while(!pq.empty()){
         auto tp = pq.top();
         pq.pop();
         int currentPoint = tp.first;
         int currentDist = tp.second;
-        if(currentPoint == to) return std::vector<std::pair<int, int > > {{to, currentDist}};
+        if(currentPoint == to) {
+            distances.clear();
+            distances.push_back(to);
+            distances.push_back(currentDist);
+            return;
+        }
         if(marked.count(currentPoint) > 0) continue;
         marked.insert(currentPoint);
-        distances.push_back({currentPoint, currentDist});
+        distances.push_back(currentPoint);
+        distances.push_back(currentDist);
         for (auto c: adj[currentPoint]){
             int nextPoint = c.first;
             int nextDist = c.second;
-            pq.push({nextPoint, currentDist + nextDist});
+            if(marked.count(nextPoint) == 0) pq.push({nextPoint, currentDist + nextDist});
         }
     }
-    if(to == -2) return distances;
-    return std::vector<std::pair<int, int > > {{to, -1}};
+    if(to == -2) return;
+    distances.clear();
+    distances.push_back(to);
+    distances.push_back(-1);
 }
--- src/addons/dijkstra.hpp
+++ src/addons/dijkstra.hpp
@@ -1,8 +1,8 @@
 #include <node_api.h>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <utility>
 
 napi_value Dijkstra(napi_env env, napi_callback_info info);
 
-std::vector<std::pair<int, int > > DijkstraImpl(std::map<int, std::map<int, int> > adj, int from, int to);
+void DijkstraImpl(std::unordered_map<int, std::vector<std::pair<int, int>>>& adj, int from, int to, std::vector<int>& distances);
